465c9de4a85a4ba77d6ce976276f5e7e135e7fc9
tragicmanner <tragicmanner@live.com>
2013-08-28 02:06:48 -0700

    Add Entine Solution
    -Full solution added
    -Functionality to see string length
    -Currently no support for text box rows/lines
    
    Program needs a widths.yml file to run. This can be placed in the same directory as the program executable. This yml file can be acquired from the Fonts directory in a CoilSnake project.
    
    Side Note: 132 pixels seems to be the limit for a single line of text before it is carried over to the next line by EarthBound's routine to automatically insert line breaks.

---
+++ EBLineParser/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace EBLineParser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.totalSizeLabel = new System.Windows.Forms.Label();
+            this.firstSizeLabel = new System.Windows.Forms.Label();
+            this.secondSizeLabel = new System.Windows.Forms.Label();
+            this.thirdSizeLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Line:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Total Size:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "First Line Size:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 158);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Second Line Size:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(27, 183);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Third Line Size:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(27, 239);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(176, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Current line does not exceed 3 rows";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(20, 25);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(252, 65);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // totalSizeLabel
+            // 
+            this.totalSizeLabel.AutoSize = true;
+            this.totalSizeLabel.Location = new System.Drawing.Point(90, 102);
+            this.totalSizeLabel.Name = "totalSizeLabel";
+            this.totalSizeLabel.Size = new System.Drawing.Size(0, 13);
+            this.totalSizeLabel.TabIndex = 8;
+            // 
+            // firstSizeLabel
+            // 
+            this.firstSizeLabel.AutoSize = true;
+            this.firstSizeLabel.Location = new System.Drawing.Point(108, 133);
+            this.firstSizeLabel.Name = "firstSizeLabel";
+            this.firstSizeLabel.Size = new System.Drawing.Size(0, 13);
+            this.firstSizeLabel.TabIndex = 9;
+            // 
+            // secondSizeLabel
+            // 
+            this.secondSizeLabel.AutoSize = true;
+            this.secondSizeLabel.Location = new System.Drawing.Point(126, 158);
+            this.secondSizeLabel.Name = "secondSizeLabel";
+            this.secondSizeLabel.Size = new System.Drawing.Size(0, 13);
+            this.secondSizeLabel.TabIndex = 10;
+            // 
+            // thirdSizeLabel
+            // 
+            this.thirdSizeLabel.AutoSize = true;
+            this.thirdSizeLabel.Location = new System.Drawing.Point(114, 183);
+            this.thirdSizeLabel.Name = "thirdSizeLabel";
+            this.thirdSizeLabel.Size = new System.Drawing.Size(0, 13);
+            this.thirdSizeLabel.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.thirdSizeLabel);
+            this.Controls.Add(this.secondSizeLabel);
+            this.Controls.Add(this.firstSizeLabel);
+            this.Controls.Add(this.totalSizeLabel);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label totalSizeLabel;
+        private System.Windows.Forms.Label firstSizeLabel;
+        private System.Windows.Forms.Label secondSizeLabel;
+        private System.Windows.Forms.Label thirdSizeLabel;
+    }
+}
+
---
+++ EBLineParser/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EBLineParser
+{
+    public partial class Form1 : Form
+    {
+        private char[] AllChars;
+        private int[] AllWidths;
+        private int lineSize;
+        private string inputFile;
+        private string AllCharsString;
+
+        public Form1()
+        {
+            inputFile = "widths.yml";
+            lineSize = 30;
+            InitializeComponent();
+            characterInitializer();
+            readWidths();
+        }
+
+        public Form1(string aFile)
+        {
+            inputFile = aFile;
+            lineSize = 30;
+            InitializeComponent();
+            characterInitializer();
+            readWidths();
+        }
+
+        public Form1(string aFile, int aSize)
+        {
+            inputFile = aFile;
+            lineSize = aSize;
+            InitializeComponent();
+            characterInitializer();
+            readWidths();
+        }
+
+        public Form1(int aSize)
+        {
+            inputFile = "widths.yml";
+            lineSize = aSize;
+            InitializeComponent();
+            characterInitializer();
+            readWidths();
+        }
+
+
+        private void characterInitializer()
+        {
+            AllChars = new char[96];
+            
+            AllChars[0] = ' ';
+            AllChars[1] = '!';
+            AllChars[2] = '"';
+            AllChars[3] = '#';
+            AllChars[4] = '$';
+            AllChars[5] = '%';
+            AllChars[6] = '&';
+            AllChars[7] = '\'';
+            AllChars[8] = '(';
+            AllChars[9] = ')';
+            AllChars[10] = '*';
+            AllChars[11] = '+';
+            AllChars[12] = ',';
+            AllChars[13] = '-';
+            AllChars[14] = '.';
+            AllChars[15] = '/';
+            AllChars[16] = '0';
+            AllChars[17] = '1';
+            AllChars[18] = '2';
+            AllChars[19] = '3';
+            AllChars[20] = '4';
+            AllChars[21] = '5';
+            AllChars[22] = '6';
+            AllChars[23] = '7';
+            AllChars[24] = '8';
+            AllChars[25] = '9';
+            AllChars[26] = ':';
+            AllChars[27] = ';';
+            AllChars[28] = '<';
+            AllChars[29] = '=';
+            AllChars[30] = '>';
+            AllChars[31] = '?';
+            AllChars[32] = '@';
+            AllChars[33] = 'A';
+            AllChars[34] = 'B';
+            AllChars[35] = 'C';
+            AllChars[36] = 'D';
+            AllChars[37] = 'E';
+            AllChars[38] = 'F';
+            AllChars[39] = 'G';
+            AllChars[40] = 'H';
+            AllChars[41] = 'I';
+            AllChars[42] = 'J';
+            AllChars[43] = 'K';
+            AllChars[44] = 'L';
+            AllChars[45] = 'M';
+            AllChars[46] = 'N';
+            AllChars[47] = 'O';
+            AllChars[48] = 'P';
+            AllChars[49] = 'Q';
+            AllChars[50] = 'R';
+            AllChars[51] = 'S';
+            AllChars[52] = 'T';
+            AllChars[53] = 'U';
+            AllChars[54] = 'V';
+            AllChars[55] = 'W';
+            AllChars[56] = 'X';
+            AllChars[57] = 'Y';
+            AllChars[58] = 'Z';
+            AllChars[59] = '[';
+            AllChars[60] = '\\';
+            AllChars[61] = ']';
+            AllChars[62] = '^';
+            AllChars[63] = '_';
+            AllChars[64] = '`';
+            AllChars[65] = 'a';
+            AllChars[66] = 'b';
+            AllChars[67] = 'c';
+            AllChars[68] = 'd';
+            AllChars[69] = 'e';
+            AllChars[70] = 'f';
+            AllChars[71] = 'g';
+            AllChars[72] = 'h';
+            AllChars[73] = 'i';
+            AllChars[74] = 'j';
+            AllChars[75] = 'k';
+            AllChars[76] = 'l';
+            AllChars[77] = 'm';
+            AllChars[78] = 'n';
+            AllChars[79] = 'o';
+            AllChars[80] = 'p';
+            AllChars[81] = 'q';
+            AllChars[82] = 'r';
+            AllChars[83] = 's';
+            AllChars[84] = 't';
+            AllChars[85] = 'u';
+            AllChars[86] = 'v';
+            AllChars[87] = 'w';
+            AllChars[88] = 'x';
+            AllChars[89] = 'y';
+            AllChars[90] = 'z';
+            AllChars[91] = '{';
+            AllChars[92] = '|';
+            AllChars[93] = '}';
+            AllChars[94] = '~';
+            AllChars[95] = '¬';
+
+            AllCharsString = charArraytoString(AllChars);
+        }
+
+        private void readWidths()
+        {
+            AllWidths = new int[96];
+            string line = "";
+            string subLine = "";
+            int counter = 0;
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(inputFile))
+                {
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        subLine = line.Substring(line.IndexOf(':') + 1, line.Length - (line.IndexOf(':') + 1));
+                        subLine = subLine.Trim();
+                        AllWidths[counter] = Convert.ToInt32(subLine);
+                        counter++;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "The file " + inputFile + " could not be read:");
+            }
+        }
+
+        private string charArraytoString(char[] chars)
+        {
+            string outString = "";
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                outString = outString + "" + chars[i];
+            }
+
+            return outString;
+        }
+
+        private int calcStringSize(string aString)
+        {
+            int totalSize = 0;
+
+            foreach (char c in aString)
+            {
+                //The size of the character being used
+                totalSize += AllWidths[AllCharsString.IndexOf(c)];
+                //The padding between characters
+                totalSize += 1;
+            }
+
+            return totalSize;
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text.Length > 0)
+            {
+                totalSizeLabel.Text = calcStringSize(richTextBox1.Text).ToString();
+            }
+            else
+            {
+                totalSizeLabel.Text = "0";
+            }
+        }
+    }
+}
